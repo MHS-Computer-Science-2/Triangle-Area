53edeb7263a26216124bf2eb41e291478de5df6a
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2022-02-11 08:11:09 -0500

    Add files via upload

---
+++ TriangleArea.cpp
@@ -0,0 +1,33 @@
+// Return Functions
+
+// I CAN write and call return functions
+
+#include <iostream>
+using namespace std;
+
+//** Functions start here **
+
+double half(double x) {
+	return x / 2;
+}
+
+double times(double a, double b) {
+	return a * b;
+}
+
+//** Functions end here **
+
+
+int main()
+{
+	//Ask the user to enter the base and height of a triangle
+
+
+	// Using only function calls, compute the area of the triangle 
+	// and display it. Area = (base * height) / 2
+	// You may NOT use any math symbols (*, /) in the main
+
+
+
+	system("pause");
+}
